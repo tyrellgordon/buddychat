2c20938b04499a0d36adc84a36c3d0efb807a8e9
doctorpillcosby <gordontyrell1993@gmail.com>
2018-10-29 17:00:54 -0400

    adding chat functionality

--- sockets/server.js
+++ sockets/server.js
@@ -1,2 +1,13 @@
 const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({port: 9090})
+
+const clients = []
+
+wss.on('connection', (connection) => {
+  clients.push(connection)
+
+  connection.on('message', (message) => {
+    const data = JSON.parse(message)
+    clients.forEach(client => client.send(JSON.stringify(data)))
+  })
+})
